83edbabeb66f7317be1040d975f05361b553208e
Shawn Chapla <shwnchpl@gmail.com>
2018-11-10 10:27:58 -0500

    Load and enable IPython autoreload extension
    Loads and enables the IPython autoreload extension by default in
    ipython_config.py. Also adds comments to ipython_config.py to help
    imrpove organization.

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,1 +1,9 @@
+# Set line editing mode to vi-mode.
 c.TerminalInteractiveShell.editing_mode = 'vi'
+
+# Load and enable the autoreload extension.
+c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.exec_lines = [
+    '%autoreload 2',
+    'print("\\n\\x1b[31mWarning:\\x1b[0m disable autoreload in ipython_config.py to improve performance.")'
+]
