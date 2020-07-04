825097e7fb5b3cdbc0ac01fbad4bcfbb4067c899
Shawn M. Chapla <shawn@chapla.email>
2020-07-04 10:21:42 -0400

    Add disp.sh script
    Adds a disp.sh script to easily switch between commonly used display
    options.

---
+++ hive/home/bin/disp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if [ "$1" = "main" ]; then
+    xrandr --output HDMI-0 --mode 1920x1080
+    xrandr --output DP-3 --off
+elif [ "$1" = "tv" ]; then
+    xrandr --output DP-3 --mode 1920x1080
+    xrandr --output HDMI-0 --off
+elif [ "$1" = "both" ]; then
+    xrandr --output HDMI-0 --mode 1920x1080
+    xrandr --output DP-3 --mode 1920x1080
+else
+    echo "usage: $0 [main|tv|both]" 1>&2
+    exit 1
+fi
