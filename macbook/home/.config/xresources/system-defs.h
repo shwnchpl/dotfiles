57562c59dfdda5824cfc8668a887f00e6c2b3dea
Shawn M. Chapla <shwnchpl@gmail.com>
2020-07-04 18:20:20 -0400

    Integrate configuration updates onto MacBook
    Integrates recent configuration updates onto my MacBook. At this point,
    most configs should be tracked in a relatively modular manner.

---
+++ macbook/home/.config/xresources/system-defs.h
@@ -0,0 +1,7 @@
+/*
+ * File: xresources/system-defs.h
+ * Author: Shawn M. Chapla
+ */
+
+#define SYSTEM_DPI              144
+#define SYSTEM_URXVT_FONT       xft:Fixedsys Excelsior 301:pixelsize=24
