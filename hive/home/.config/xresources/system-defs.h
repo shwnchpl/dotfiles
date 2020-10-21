d13934819129fb57fb534a16c09e8728b0c9ba89
Shawn M. Chapla <shawn@chapla.email>
2020-10-21 18:37:36 -0400

    Make additional URxvt options system specific
    Makes LetterSpace, LineSpace, and Bold Font system specific in
    .Xresources. Adds system specific definitions for hive and macbook
    configuration profiles to maintain current settings.

--- hive/home/.config/xresources/system-defs.h
+++ hive/home/.config/xresources/system-defs.h
@@ -3,5 +3,8 @@
  * Author: Shawn M. Chapla
  */
 
-#define SYSTEM_DPI              96
-#define SYSTEM_URXVT_FONT       xft:Fixedsys Excelsior 301:pixelsize=16
+#define SYSTEM_DPI                  96
+#define SYSTEM_URXVT_FONT           xft:Fixedsys Excelsior 301:pixelsize=16
+#define SYSTEM_URXVT_BOLD_FONT      xft:Fixedsys Excelsior 301:pixelsize=16
+#define SYSTEM_URXVT_LETTERSPACE    -1
+#define SYSTEM_URXVT_LINESPACE      1
--- macbook/home/.config/xresources/system-defs.h
+++ macbook/home/.config/xresources/system-defs.h
@@ -3,5 +3,8 @@
  * Author: Shawn M. Chapla
  */
 
-#define SYSTEM_DPI              144
-#define SYSTEM_URXVT_FONT       xft:Fixedsys Excelsior 301:pixelsize=24
+#define SYSTEM_DPI                  144
+#define SYSTEM_URXVT_FONT           xft:Fixedsys Excelsior 301:pixelsize=24
+#define SYSTEM_URXVT_BOLD_FONT      xft:Fixedsys Excelsior 301:pixelsize=24
+#define SYSTEM_URXVT_LETTERSPACE    -1
+#define SYSTEM_URXVT_LINESPACE      1
