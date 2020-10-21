aeadc89f6104341d15eff8345fee300117cd5de4
Shawn M. Chapla <shawn@chapla.email>
2020-10-21 18:38:43 -0400

    Update hive URxvt font settings
    Updates hive URxvt font settings to use "Bitstream Vera Sans Mono"
    rather than "Fixedsys Excelsior." These font settings look relatively
    similar to those used by Konsole and are, in my opinion, rather sharp.

--- hive/home/.config/xresources/system-defs.h
+++ hive/home/.config/xresources/system-defs.h
@@ -4,7 +4,7 @@
  */
 
 #define SYSTEM_DPI                  96
-#define SYSTEM_URXVT_FONT           xft:Fixedsys Excelsior 301:pixelsize=16
-#define SYSTEM_URXVT_BOLD_FONT      xft:Fixedsys Excelsior 301:pixelsize=16
+#define SYSTEM_URXVT_FONT           xft:Bitstream Vera Sans Mono:size=10
+#define SYSTEM_URXVT_BOLD_FONT      xft:Bitstream Vera Sans Mono:style=Bold:size=10
 #define SYSTEM_URXVT_LETTERSPACE    -1
-#define SYSTEM_URXVT_LINESPACE      1
+#define SYSTEM_URXVT_LINESPACE      0
