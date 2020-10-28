e32ae12c727037211c25b76168b4b5bebe9f21f4
Shawn M. Chapla <shawn@chapla.email>
2020-10-27 22:01:55 -0400

    Activate hive dual desktop mode with Super+F9
    Adds a Super+F9 keybinding and the relevant lines to disp.sh to activate
    dual desktop mode (shared desktop across two monitors).

--- hive/home/bin/disp.sh
+++ hive/home/bin/disp.sh
@@ -11,6 +11,9 @@ elif [ "$1" = "tv" ]; then
 elif [ "$1" = "both" ]; then
     xrandr --output HDMI-0 --primary --mode 1920x1080
     xrandr --output DP-3 --mode 1920x1080
+elif [ "$1" = "dual" ]; then
+    xrandr --output DP-3 --mode 1920x1080
+    xrandr --output HDMI-0 --mode 1920x1080 --primary --right-of DP-3
 else
     echo "usage: $0 [main|tv|both]" 1>&2
     exit 1
