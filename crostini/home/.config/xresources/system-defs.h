7a7268b950d7f90c19740ab549ed96ba3746a0ed
Shawn M. Chapla <shawn@chapla.email>
2021-04-11 14:29:52 -0400

    Add Crostini module
    Adds a module with Crostini/ChromeOS specific system configs.

---
+++ crostini/home/.config/xresources/system-defs.h
@@ -0,0 +1,10 @@
+/*
+ * File: xresources/system-defs.h
+ * Author: Shawn M. Chapla
+ */
+
+#define SYSTEM_DPI                  144
+#define SYSTEM_URXVT_FONT           xft:Noto Sans Mono:size=16
+#define SYSTEM_URXVT_BOLD_FONT      xft:Noto Sans Mono:style=Bold:size=16
+#define SYSTEM_URXVT_LETTERSPACE    -1
+#define SYSTEM_URXVT_LINESPACE      0
---
+++ crostini/home/README.rst
@@ -0,0 +1,5 @@
+Crostini Specific Software
+==========================
+
+* fcitx (configure w/ fcitx-config-gtk3)
+* fcitx-anthy
