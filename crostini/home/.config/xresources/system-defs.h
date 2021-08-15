d44cb57f51853be701b61cf6e70f1f96227699fd
Shawn M. Chapla <shawn@chapla.email>
2021-08-15 08:46:09 -0400

    Update Crostini terminal font

--- crostini/home/.config/xresources/system-defs.h
+++ crostini/home/.config/xresources/system-defs.h
@@ -4,7 +4,7 @@
  */
 
 #define SYSTEM_DPI                  144
-#define SYSTEM_URXVT_FONT           xft:Noto Sans Mono:size=16
-#define SYSTEM_URXVT_BOLD_FONT      xft:Noto Sans Mono:style=Bold:size=16
+#define SYSTEM_URXVT_FONT           xft:Bitstream Vera Sans Mono:size=16
+#define SYSTEM_URXVT_BOLD_FONT      xft:Bitstream Vera Sans Mono:style=Bold:size=16
 #define SYSTEM_URXVT_LETTERSPACE    -1
 #define SYSTEM_URXVT_LINESPACE      0
