389ce50725c9acf01653da08a62422e2e2f094dc
Shawn M. Chapla <shawn@chapla.email>
2022-05-24 13:02:52 -0400

    disp.sh: Add explicit rate to xrandr invocations

--- hive/home/bin/disp.sh
+++ hive/home/bin/disp.sh
@@ -3,17 +3,17 @@
 set -e
 
 if [ "$1" = "main" ]; then
-    xrandr --output HDMI-0 --primary --mode 1920x1080
+    xrandr --output HDMI-0 -r 60 --primary --mode 1920x1080
     xrandr --output DP-3 --off
 elif [ "$1" = "tv" ]; then
     xrandr --output DP-3 --primary --mode 1920x1080
     xrandr --output HDMI-0 --off
 elif [ "$1" = "both" ]; then
-    xrandr --output HDMI-0 --primary --mode 1920x1080
+    xrandr --output HDMI-0 -r 60 --primary --mode 1920x1080
     xrandr --output DP-3 --mode 1920x1080
 elif [ "$1" = "dual" ]; then
     xrandr --output DP-3 --mode 1920x1080
-    xrandr --output HDMI-0 --mode 1920x1080 --primary --right-of DP-3
+    xrandr --output HDMI-0 -r 60 --mode 1920x1080 --primary --right-of DP-3
 else
     echo "usage: $0 [main|tv|both]" 1>&2
     exit 1
