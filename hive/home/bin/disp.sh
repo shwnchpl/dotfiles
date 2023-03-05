95b6a6102c8a3b1305f3ccf17155c8802ad0ba05
Shawn M. Chapla <shawn@chapla.email>
2023-03-05 01:36:30 -0500

    hive: Update disp.sh for new monitor config

--- hive/home/bin/disp.sh
+++ hive/home/bin/disp.sh
@@ -3,18 +3,18 @@
 set -e
 
 if [ "$1" = "main" ]; then
-    xrandr --output HDMI-0 -r 60 --primary --mode 1920x1080
-    xrandr --output DP-3 --off
-elif [ "$1" = "tv" ]; then
     xrandr --output DP-3 --primary --mode 1920x1080
     xrandr --output HDMI-0 --off
+elif [ "$1" = "tv" ]; then
+    xrandr --output HDMI-0 --primary --mode 1920x1080
+    xrandr --output DP-3 --off
 elif [ "$1" = "both" ]; then
-    xrandr --output HDMI-0 -r 60 --primary --mode 1920x1080
-    xrandr --output DP-3 --mode 1920x1080
+    xrandr --output DP-3 --primary --mode 1920x1080
+    xrandr --output HDMI-0 --mode 1920x1080 --same-as DP-3
 elif [ "$1" = "dual" ]; then
-    xrandr --output DP-3 --mode 1920x1080
-    xrandr --output HDMI-0 -r 60 --mode 1920x1080 --primary --right-of DP-3
+    xrandr --output DP-3 --primary --mode 1920x1080
+    xrandr --output HDMI-0 --mode 1920x1080 --left-of DP-3
 else
-    echo "usage: $0 [main|tv|both]" 1>&2
+    echo "usage: $0 [main|tv|both|dual]" 1>&2
     exit 1
 fi
